6d4d818505f38e7aefd5e7be6f28d6887c7f014b
Igor Vaiman <igor.vaiman@bostongene.com>
2020-07-20 03:14:54 +0300

    Initial commit: selection, extraction, merging

---
+++ lowstate_extraction.py
@@ -0,0 +1,75 @@
+"""
+Extraction of specific time interval from large Fermi data set.
+
+Module automates config creation and runs fermitools utils.
+"""
+
+import os
+import yaml
+import datetime
+
+from fermipy.gtanalysis import GTAnalysis
+
+
+BASE_CONFIG = '/home/njvh/Fermi_data/PKS1510-089/config_5day_binning.yaml'
+
+
+def set_base_config(newbaseconfig):
+    """
+    To change base config from outside
+    """
+    global BASE_CONFIG
+    BASE_CONFIG = newbaseconfig
+
+
+def prepare_temp_config(tmin, tmax, outdir=None, find_directory=False):
+    with open(BASE_CONFIG) as file:
+        config = yaml.load(file)
+
+    if outdir is None:
+        outdir = 'out_temp_from_{}_to_{}'.format(int(tmin), int(tmax))
+    outdir = os.path.join(os.getcwd(), outdir)
+    if os.path.exists(outdir):
+        if not find_directory:
+            raise OSError('Outdir \'{}\' already exists!'.format(outdir))
+        version = 2
+        while os.path.exists(outdir + '_v{}'.format(version)):
+            version += 1
+        outdir = outdir + '_v{}'.format(version)
+    os.mkdir(outdir)
+
+    config['fileio'] = dict()
+    config['fileio']['outdir'] = outdir
+    config['selection']['tmin'] = tmin
+    config['selection']['tmax'] = tmax
+
+    config_path = os.path.join(outdir, 'temp_config.yaml')
+
+    with open(config_path, 'w') as f:
+        f.write(
+            '# Automatically created from \'{}\' on {}\n\n'
+            .format(BASE_CONFIG, datetime.datetime.now())
+        )
+        yaml.dump(config, f, indent=4)
+
+    return config_path, outdir
+
+
+def extract_photons_data(tmin, tmax, outdir=None, find_directory=False):
+    """Perform GTAnalysis setup with custom tmin and tmax.
+    For details see PKS notebook"""
+    config_path, outdir = prepare_temp_config(
+        tmin, tmax, outdir, find_directory
+    )
+
+    gta = GTAnalysis(config_path, logging={'verbosity': 3})
+    gta.setup()
+
+    return outdir
+
+
+if __name__ == "__main__":
+    # config_path, outdir = prepare_temp_config(0, 0)
+    # with open(config_path) as f:
+    #     print(yaml.load(f))
+    extract_photons_data(244093400.0, 252733400.0)
---
+++ lowstate_merging.py
@@ -0,0 +1,88 @@
+"""Merging several time epochs, specified by MET, into one fermipy config
+
+This script reads time intervals (e.g. produced in lowstate_selection.ipynb),
+extracts data for each one with lowstate_extraction.py helper module and
+merges them into one config, ready for GTAnalysis
+"""
+
+import os
+import subprocess
+import yaml
+
+from fermipy.gtanalysis import GTAnalysis
+
+from lowstate_extraction import extract_photons_data, BASE_CONFIG
+
+
+EPOCHS_FILE = 'lowstate_MET_times.txt'  # from lowstate_selection.ipynb
+
+LTCUBE_FINAL_FILE = 'ltcubes_merged.fits'
+FT1_FILES_LIST = 'ft1_merged.lst'
+CONFIG_FINAL_FILE = 'config_merged.yaml'
+
+
+def join_ltcubes(outdirs):
+    ltcubes_list = 'ltcubes_temp.lst'
+    with open(ltcubes_list, 'w') as f:
+        for od in outdirs:
+            f.write(
+                os.path.join(od, 'ltcube_00.fits') + '\n'
+            )
+
+    cmd = 'gtltsum infile1={} outfile={}'.format(
+        ltcubes_list, LTCUBE_FINAL_FILE
+    )
+    print(subprocess.check_output(cmd, shell=True))
+
+
+if __name__ == "__main__":
+    outdirs = []
+    with open(EPOCHS_FILE) as f:
+        for line in f:
+            start, end = [float(MET) for MET in line.split()]
+            outdir = extract_photons_data(start, end)
+            outdirs.append(outdir)
+
+    # testing
+    # outdirs = [
+    #     '/home/njvh/Fermi/PKS1510-089/out_temp_from_239773400_to_242365400',
+    #     '/home/njvh/Fermi/PKS1510-089/out_temp_from_244093400_to_252733400'
+    # ]
+
+    join_ltcubes(outdirs)  # generate joined ltcube from calculated
+
+    # create list of photons (ft1) files, outputted by extractor
+    with open(FT1_FILES_LIST, 'w') as f:
+        for od in outdirs:
+            f.write(
+                os.path.join(od, 'ft1_00.fits') + '\n'
+            )
+
+    # modify base config to include merged files
+    with open(BASE_CONFIG) as infile, \
+            open(CONFIG_FINAL_FILE, 'w') as outfile:
+        config = yaml.load(infile)
+        config['data']['evfile'] = os.path.join(os.getcwd(), FT1_FILES_LIST)
+        config['data']['ltcube'] = os.path.join(os.getcwd(), LTCUBE_FINAL_FILE)
+        config['fileio'] = {'outdir': 'out_merged/'}
+        outfile.write('# Automatically merged from directories:\n')
+        for outdir in outdirs:
+            outfile.write('# {}\n'.format(outdir))
+        outfile.write('\n')
+        yaml.dump(config, outfile, indent=4)
+
+    # some generic processing just for sanity check
+    gta = GTAnalysis(CONFIG_FINAL_FILE, logging={'verbosity': 3})
+    gta.setup()
+    gta.free_source('4FGL J1512.8-0906', free=True, pars=['Index'])
+    gta.free_source('4FGL J1512.8-0906', free=True, pars='norm')
+    # Free Normalization of all Sources within 3 deg of ROI center
+    gta.free_sources(distance=3.0, pars='norm')
+    # Free all parameters of isotropic and galactic diffuse components
+    gta.free_source('galdiff')
+    gta.free_source('isodiff')
+    gta.optimize()
+    gta.print_roi()
+    fit_res = gta.fit()
+    print('Fit Quality: ', fit_res['fit_quality'])
+    print(gta.roi['4FGL J1512.8-0906'])
